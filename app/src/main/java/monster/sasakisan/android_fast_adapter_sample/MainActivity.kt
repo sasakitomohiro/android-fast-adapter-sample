18a3eb2b04c708755d551d4cd6aa153f112aefea
sasakitomohiro <t0m0.piii.12.07sk@gmail.com>
2019-08-02 23:44:51 +0900

    first commit

---
+++ app/src/main/java/monster/sasakisan/android_fast_adapter_sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package monster.sasakisan.android_fast_adapter_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import monster.sasakisan.android_fast_adapter_sample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+}
